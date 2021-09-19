2ea0e5acf978e6f462c46b1d403dfe2cbacaadd8
Insaf <insafanas@mail.ru>
2021-09-19 13:54:31 +0300

    created a calculator blank

---
+++ app/src/main/java/com/itis/androidlabproject/Engineerable.kt
@@ -0,0 +1,7 @@
+package com.itis.androidlabproject
+
+interface Engineerable {
+    fun calcLog (a: Double, b: Double) : Double
+    fun sin(a: Double) : Double
+    fun cos(a: Double) : Double
+}
---
+++ app/src/main/java/com/itis/androidlabproject/EngineeringCalculator.kt
@@ -0,0 +1,23 @@
+package com.itis.androidlabproject
+
+class EngineeringCalculator (
+    expression: String
+) : Calculator(expression), Engineerable {
+    override fun calculate () {
+
+    }
+
+    override fun calcLog(a: Double, b: Double): Double {
+        TODO("Not yet implemented")
+    }
+
+    override fun sin(a: Double): Double {
+        TODO("Not yet implemented")
+    }
+
+    override fun cos(a: Double): Double {
+        TODO("Not yet implemented")
+    }
+
+
+}
