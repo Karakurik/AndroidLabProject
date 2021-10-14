a60ce6933fbfbda386f8be90c3508d5f6e02f4a8
Insaf <insafanas@mail.ru>
2021-10-14 18:24:24 +0300

    made intent

--- app/src/main/java/com/itis/androidlabproject/MainActivity.kt
+++ app/src/main/java/com/itis/androidlabproject/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.itis.androidlabproject
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.itis.androidlabproject.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val REQUEST_CODE_1 = 123;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        with(binding) {
+            btnText.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+                intent.putExtra(Intent.EXTRA_TEXT, "test text")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivityForResult(intent, 1)
+//                    startActivity(intent) /* не работает*/
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        if (resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, "Успешный возврат", Toast.LENGTH_LONG).show()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
     }
 }
